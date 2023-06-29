1b6e0da55bd2d8d39d122f05a8c5fe53388900b9
Ndifreke Matthew Essien <31422222+Starlyvil@users.noreply.github.com>
2023-06-29 15:24:25 +0000

    Update test.groovy

--- src/test.groovy
+++ src/test.groovy
@@ -1,6 +1,8 @@
-def browsers = ['chrome', 'firefox']
-for (int i = 0; i < browsers.size(); ++i) {
-  echo "Testing the ${browsers[i]} browser"
-}
+def give(){
+  def browsers = ['chrome', 'firefox']
+  for (int i = 0; i < browsers.size(); ++i) {
+    echo "Testing the ${browsers[i]} browser"
+  }
+} 
 
 //return this
